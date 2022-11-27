a893774d5ca5257fd1d0a100da7cb2bd1adc0e5e
ch3wb4cc4 <44432818+ch3wb4cc4@users.noreply.github.com>
2022-11-27 13:42:08 +0100

    only the essentials for now

--- metadata.php
+++ metadata.php
@@ -1,26 +1,26 @@
 <?php
 /**
- * This file is part of OXID eShop Logger Demo module.
- * Logger Demo module is free software:
+ * This file is part of MS Product Volume module.
+ * MS Product Volume module is free software:
  * you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  *
- * Logger Demo module is distributed in
+ * MS Product Volume module is distributed in
  * the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OXID eShop Logger Demo module.
+ * along with MS Product Volume module.
  * If not, see <http://www.gnu.org/licenses/>.
  *
  * @category      module
- * @package       loggerdemo
+ * @package       msproductvolume
  * @author        OXID eSales AG
  * @link          http://www.oxid-esales.com/en/
- * @copyright (C) OXID e-Sales, 2003-2015
+ * @copyright (C) MS, 2022-2032
  */
 
 /**
@@ -32,29 +32,27 @@ $sMetadataVersion = '2.0';
  * Module information
  */
 $aModule = array(
-    'id'          => 'oeloggerdemo',
+    'id'          => 'msproductvolume',
     'title'       => [
-        'de' => 'OE Logger Demo',
-        'en' => 'OE Logger Demo',
+        'de' => 'MS Product Volume',
+        'en' => 'MS Product Volume',
     ],
     'description' => [
-        'de' => 'OE Logger Demo Module',
-        'en' => 'OE Logger Demo Module',
+        'de' => 'MS Product Volume Module',
+        'en' => 'MS Product Volume Module',
     ],
     'thumbnail'   => 'out/pictures/picture.png',
-    'version'     => '2.0.0',
-    'author'      => 'OXID eSales AG',
-    'url'         => 'http://www.oxid-esales.com/en/',
-    'email'       => 'mantas.vaitkunas@oxid-esales.com',
+    'version'     => '1.0.0a',
+    'author'      => 'MS',
+    'url'         => '',
+    'email'       => '',
     'extend'      => [
-        \OxidEsales\Eshop\Application\Model\Basket::class => \OxidEsales\LoggerDemo\Model\Basket::class,
     ],
-    'settings'    => [
+	'blocks'      => [
         [
-            'group' => 'logger',
-            'name'  => 'sLoggerInput',
-            'type'  => 'str',
-            'value' => 'default',
-        ],
-    ],
+            'template' => 'page/details/inc/productmain.tpl',
+            'block'    => 'details_productmain_shortdesc',
+            'file'     => '/views/blocks/details_productmain_shortdesc.tpl',
+        ]
+    ]
 );
